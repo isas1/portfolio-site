79462cf9e7056da0e0e888ec42654e18917988fb
isas1 <teachmecomputerscience@gmail.com>
2018-08-22 09:15:50 +0100

    Styles not working

--- src/Components/SocialIcons/SocialIcons.js
+++ src/Components/SocialIcons/SocialIcons.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
 import { FaGithub } from 'react-icons/fa';
+import { FaSpaceShuttle } from 'react-icons/fa';
+import { IconContext } from "react-icons";
 
-import './style.css';
+const iconStyle = {
+  size: '3em',
+  border: '5px solid pink'
+};
 
-const SocialIcons = (props, context) => {
-  //const { theme: { colorPrimary } } = context;
+const SocialIcons = () => {
   
   return (
-    <div className="social-icons animate-icons">
-        <a target="_blank" rel="noopener noreferrer" href="https://github.com/isas1" ><FaGithub /></a>
-        {/*<a target="_blank" rel="noopener noreferrer" href="https://twitter.com/flexdinesh" ><i className="fab fa-twitter">Twitter</i></a>
+    <IconContext.Provider value={{ style: {iconStyle}}}>
+        <a href="https://github.com/isas1" ><FaGithub /></a>
+        <a href="https://isas1.github.io" ><FaSpaceShuttle /></a>
+        {/*
         <a target="_blank" rel="noopener noreferrer" href="https://stackoverflow.com/story/flexdinesh" ><i className="fab fa-stack-overflow">Stack Overflow</i></a>
         <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/dineshpandiyan" ><i className="fab fa-linkedin">LinkedIn</i></a>
         <a target="_blank" rel="noopener noreferrer" href="https://medium.com/@flexdinesh" ><i className="fab fa-medium"></i></a>
-  <a className="devto" target="_blank" rel="noopener noreferrer" href="https://dev.to/flexdinesh"></a>*/}
-      </div>
+        <a className="devto" target="_blank" rel="noopener noreferrer" href="https://dev.to/flexdinesh"></a>
+        */}
+      </IconContext.Provider>
   );
 };
 
