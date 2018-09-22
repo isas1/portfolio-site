8f353a59871aa48759992c752777bf2d50e9d2ae
isas1 <teachmecomputerscience@gmail.com>
2018-09-22 20:21:45 +0100

    Explore page added

--- src/Pages/Explore/Explore.jsx
+++ src/Pages/Explore/Explore.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import ReactAutocomplete from 'react-autocomplete';
+import './main.js';
 
 export default class Explore extends Component {
 
@@ -9,29 +9,10 @@ export default class Explore extends Component {
       value: '',
     }
   }
-
   render() {
     return (
-      <ReactAutocomplete
-        items={[
-          { id: 'foo', label: 'foo' },
-          { id: 'bar', label: 'bar' },
-          { id: 'baz', label: 'baz' },
-        ]}
-        shouldItemRender={(item, value) => item.label.toLowerCase().indexOf(value.toLowerCase()) > -1}
-        getItemValue={item => item.label}
-        renderItem={(item, highlighted) =>
-          <div
-            key={item.id}
-            style={{ backgroundColor: highlighted ? '#eee' : 'transparent'}}
-          >
-            {item.label}
-          </div>
-        }
-        value={this.state.value}
-        onChange={e => this.setState({ value: e.target.value })}
-        onSelect={value => this.setState({ value })}
-      />
+      <canvas id="draw" width="800" height="800">
+      </canvas>
     )
   }
 }
